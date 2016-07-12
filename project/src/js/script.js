4581af37c20b2c57bd5c4f9ea329fe0d2debb653
Dariusz Franczak <darius32@op.pl>
2016-07-12 02:55:34 +0100

    1st commit - frames 1 and 2 completed.

---
+++ project/src/js/script.js
@@ -0,0 +1,58 @@
+function init() {
+
+ var stage = new createjs.Stage("demoCanvas");
+ var background = new createjs.Bitmap("images/frame12-background.png");
+ var skyLogo = new createjs.Bitmap("images/frame12-sky-logo.png");
+ var products = new createjs.Bitmap("images/frame1-products.png");
+ var frame1gradientText = new createjs.Bitmap("images/frame1-gradient-tex.png");
+ var frame1blueText = new createjs.Bitmap("images/frame1-blue-text.png");
+ var frame2gradientText = new createjs.Bitmap("images/frame2-gradient-text.png");
+ var frame2greyText = new createjs.Bitmap("images/frame2-grey-text.png");
+ var frame2stamp = new createjs.Bitmap("images/frame2-stamp.png");
+ var frame3button = new createjs.Bitmap("images/frame3-button.png");
+ var frame3gradientText = new createjs.Bitmap("images/frame3-gradient-text.png");
+ var frame3blueText = new createjs.Bitmap("images/frame3-blue-text.png");
+ var frame3ltdOffer = new createjs.Bitmap("images/frame3-ltd-offer.png");
+ var frame3greyText = new createjs.Bitmap("images/frame3-grey-text.png");
+
+ skyLogo.y = 200;
+ products.y = 110;
+ frame1blueText.y = 45;
+ frame1gradientText.alpha = 0;
+ frame1blueText.alpha = 0;
+ frame2gradientText.alpha = 0;
+ frame2greyText.alpha = 0;
+ frame2greyText.y = 210;
+ frame2stamp.y = -150;
+ frame3button.y = 185;
+ frame3button.alpha = 0;
+
+ stage.addChild(background);
+ stage.addChild(skyLogo);
+ stage.addChild(products);
+ stage.addChild(frame1gradientText);
+ stage.addChild(frame1blueText);
+ stage.addChild(frame2gradientText);
+ stage.addChild(frame2greyText);
+ stage.addChild(frame2stamp);
+ stage.addChild(frame3button);
+
+ createjs.Ticker.addEventListener("tick", handleTick);
+
+ function handleTick(event) {
+    
+     stage.update();
+     // frame 1 animation
+     createjs.Tween.get(frame1gradientText).to({alpha: 1},1000).wait(3000).to({alpha: 0},1000);
+     createjs.Tween.get(frame1blueText).wait(1000).to({alpha: 1},1000).wait(2000).to({alpha: 0},1000);
+     createjs.Tween.get(products).wait(4000).to({alpha: 0},1000);
+     // frame 2 animation
+     createjs.Tween.get(frame2gradientText).wait(5000).to({alpha: 1},1000).wait(3000).to({alpha: 0},1000);
+     createjs.Tween.get(frame2greyText).wait(6000).to({alpha: 1},1000).wait(2000).to({alpha: 0},1000);
+     createjs.Tween.get(frame2stamp).wait(6000).to({x:0}, 1000, createjs.Ease.quadOut).wait(2000).to({alpha: 0},1000);
+		 createjs.Tween.get(frame2stamp).wait(6000).to({y:60}, 1000, createjs.Ease.bounceOut).wait(2000).to({alpha: 0},1000);
+		 // frame 3 animation
+		 createjs.Tween.get(frame3button).wait(10000).to({alpha: 1});
+ }
+
+ }
